fa6208ea2782031daeeb58a1d2d5cc7869f0ee58
ikz <68614343+noe999x@users.noreply.github.com>
2023-01-07 19:44:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from os import system as sis
 from time import sleep as turu
 sis('git pull')
 sis('clear')
-def asu:
+def asu():
     x=input('Sudahkah anda menginstall module yang dibutuhkan? (s/b)? : ')
     if x in['s','sudah','udh','udah']:
        import insta
