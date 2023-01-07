9c582f436f2c5b6a31c30d3b5f37fd300b211ea5
ikz <68614343+noe999x@users.noreply.github.com>
2023-01-07 19:42:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from os import system as sis
 from time import sleep as turu
 sis('git pull')
 sis('clear')
-x=input('Sudahkah anda menginstall module yang dibutuhkan? (s/b) ?')
-if x in['s','sudah','udh','udah']:
-   import insta
-elif x in['b','belum','blm','belom']:
-     sis('pip install play-audio')
-     sis('pip install requests')
-     print('Module has installed')
-     turu(2)
-     import insta
+def asu:
+    x=input('Sudahkah anda menginstall module yang dibutuhkan? (s/b) ?')
+    if x in['s','sudah','udh','udah']:
+       import insta
+    elif x in['b','belum','blm','belom']:
+         sis('pip install play-audio')
+         sis('pip install requests')
+         print('Module has installed')
+         turu(2)
+         import insta
+    elif x in['',' ']:
+         print('yang bener asu')
+         turu(2)
+         asu()
+    else:print('yang bener asu');turu(2);asu()
+asu()
+     
