b78647ae728533874b3ea4ae9ba713428ce6aa7f
ikz <68614343+noe999x@users.noreply.github.com>
2023-01-07 19:39:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import insta
+import os, time
+from os import system as sis
+from time import sleep as turu
+sis('git pull')
+sis('clear')
+x=input('Sudahkah anda menginstall module yang dibutuhkan? (s/b) ?')
+if x in['s','sudah','udh','udah']:
+   import insta
+elif x in['b','belum','blm','belom']:
+     sis('pip install play-audio')
+     sis('pip install requests')
+     print('Module has installed')
+     turu(2)
+     import insta
