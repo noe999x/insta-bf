e0e4e57bcb973ebf7401bedcd1fcf0340ee3fb7d
ikz <68614343+noe999x@users.noreply.github.com>
2023-01-08 16:31:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-#Coder : noe999x
-import os, time
+import os
 from os import system as sis
-from time import sleep as turu
-sis('clear')
-def asu():
-    x=input('Sudahkah anda menginstall module yang dibutuhkan? (s/b)? : ')
-    if x in['s','sudah','udh','udah']:
-       sis('git pull')
-       import insta
-    elif x in['b','belum','blm','belom']:
-         sis('pkg install play-audio')
-         sis('pip install requests')
-         print('Module has installed')
-         turu(2)
-         sis('git pull')
-         import insta
-    elif x in['',' ']:
-         print('yang bener asu')
-         turu(2)
-         asu()
-    else:print('yang bener asu');turu(2);asu()
-asu()
+try:
+    sis('git pull')
+except requests.exceptions.ConnectionError:
+    print('cek koneksi lo')
+import insta
