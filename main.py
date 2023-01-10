f11df706f46ecffadc981da0727115fc971550f5
ikz <68614343+noe999x@users.noreply.github.com>
2023-01-10 13:17:02 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import os, time
+from time import sleep as turu
+from os import system as sis
+sis('clear')
+print('1. Russian\n2. English\n3. Bahasa Indonesia\n0. Install bahan')
+x=input('Pilih methode bahasa : ')
+if x in['1']:
+   sis('git pull')
+   import insta_ru
+elif x in['2']:
+     sis('git pull')
+     import insta_en
+elif x in['3']:
+     sis('git pull')
+     print('•Bahasa belum tersedia')
+     exit()
+elif x in['0']:
+     sis('pkg install play-audio')
+else:print('yang bener su');turu(2);sis('python main.py')
