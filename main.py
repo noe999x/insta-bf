5d01ae7dad96d9479e422509953cfbc15f67fd13
ikz <68614343+noe999x@users.noreply.github.com>
2023-01-12 19:08:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import os, time
-from time import sleep as turu
-from os import system as sis
-sis('clear')
-print('1. Russian\n2. English\n3. Bahasa Indonesia\n0. Install bahan')
-x=input('Pilih bahasa : ')
-if x in['1']:
-   sis('git pull')
-   import insta_ru
-elif x in['2']:
-     sis('git pull')
-     import insta_en
-elif x in['3']:
-     sis('git pull')
-     print('•Bahasa belum tersedia')
-     exit()
-elif x in['0']:
-     sis('pkg install play-audio')
-else:print('yang bener su');turu(2);sis('python main.py')
+from time import sleep as barbara
+from os import system as hutao
+def susu_ganyu():
+    hutao('clear')
+    print(f""" ⢀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀                                                                  
+⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠉⢹⣿⣆  ___   _  _   ___   _____   _     ___   ___     _     __  __     
+⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠉⠉⠛⠻⢿⣿⣿⣷⣶⣾⣿⣿ |_ _| | \| | / __| |_   _| /_\   / __| | _ \   /_\   |  \/  |    
+⣿⣿⣿⣿⣿⡿⠋⠀⠀⣀⣠⣄⣀⠀⠀⠙⢿⣿⣿⣿⣿⣿  | |  | .` | \__ \   | |  / _ \ | (_ | |   /  / _ \  | |\/| |    
+⣿⣿⣿⣿⣿⠁⠀⢠⣾⣿⣿⣿⣿⣷⡄⠀⠈⣿⣿⣿⣿⣿ |___| |_|\_| |___/   |_| /_/ \_\ \___| |_|_\ /_/ \_\ |_|  |_|    
+⣿⣿⣿⣿⡇⠀⠀⢾⣿⣿⣿⣿⣿⣿⡷⠀⠀⢸⣿⣿⣿⣿ Codder : noe999x
+⣿⣿⣿⣿⣿⡀⠀⠘⢿⣿⣿⣿⣿⡿⠃⠀⢀⣿⣿⣿⣿⣿ Special thanks to : BrayennnXD - DerrXyr
+⣿⣿⣿⣿⣿⣷⣄⠀⠀⠉⠙⠋⠉⠀⠀⣠⣾⣿⣿⣿⣿⣿ Last updated on github : 12-01-2023 [FinalVer]
+⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣀⣀⣤⣴⣾⣿⣿⣿⣿⣿⣿⣿ ••••••••••••••••••••••••
+⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏ facebook.com/bagasekaapr
+⠀⠈⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀ github.com/noe999x""")
+def susu_nahida():
+    susu_ganyu()
+    print('1. Russian\n2. English\n3. Bahasa Indonesia\n0. Update SC')
+    lumine=input('Mulai program dengan bahasa : ')
+    if lumine in['1']:
+       import insta_ru
+    elif lumine in['2']:
+         import insta_en
+    elif lumine in['3']:
+         print('• Bahasa belum tersedia');barbara(2)
+         susu_nahida()
+    elif lumine in['0']:
+         hutao('git pull')
+         hutao('pkg install play-audio')
+    else:print('• Yang bener su');barbara(2);hutao('python main.py')
+susu_nahida()
